e27ff0f422bb300d94527b68aac3c46f164fad31
Brandon Butler <85582593+BrandonButler123@users.noreply.github.com>
2024-01-20 15:09:05 -0330

    updating spacing

--- os.js
+++ os.js
@@ -1,8 +1,8 @@
 // The operaitng system (os) module is a core Node.js module that provides functionality to interact
-//with the underlying operating system, offering methods to access information such as platform, architecture,
-//user details, memory usage, CPU information, and network interfaces. By utilizing the os module, developers
-//can retrieve essential system-related data for building applications that require insights into the runtime
-//environment, facilitating platform-specific optimizations and providing a foundation for system-level functionalities.
+// with the underlying operating system, offering methods to access information such as platform, architecture,
+// user details, memory usage, CPU information, and network interfaces. By utilizing the os module, developers
+// can retrieve essential system-related data for building applications that require insights into the runtime
+// environment, facilitating platform-specific optimizations and providing a foundation for system-level functionalities.
 
 // intialize the os module
 const os = require("os");
